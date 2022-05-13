c3be2b820f5d1318edae15c97fb4ac0e0ef62eb2
smayerv <smayer@varicent.com>
2022-05-13 12:21:00 -0600

    log some info

--- tap_dynamics/sync.py
+++ tap_dynamics/sync.py
@@ -5,13 +5,15 @@ from tap_dynamics.streams import get_streams
 
 LOGGER = singer.get_logger()
 
+
 def sync(config, config_path, state, catalog):
     """ Sync data from tap source """
 
     streams = get_streams(config, config_path)
     # TODO: document that newly created fields won't be selected as currently implemented
 
-    LOGGER.info('There are {:d} valid streams in MS Dynamics'.format(len(streams)))
+    LOGGER.info(
+        'There are {:d} valid streams in MS Dynamics'.format(len(streams)))
 
     with Transformer() as transformer:
         for stream in catalog.get_selected_streams(state):
@@ -19,7 +21,7 @@ def sync(config, config_path, state, catalog):
             stream_obj = streams[tap_stream_id]
             stream_schema = stream.schema.to_dict()
             stream_metadata = metadata.to_map(stream.metadata)
-
+            LOGGER.info(stream)
             LOGGER.info('Starting sync for stream: %s', tap_stream_id)
 
             state = singer.set_currently_syncing(state, tap_stream_id)
@@ -32,7 +34,9 @@ def sync(config, config_path, state, catalog):
                 stream.replication_key
             )
 
-            state = stream_obj.sync(state, stream_schema, stream_metadata, config, transformer)
+            state = stream_obj.sync(
+                state, stream_schema, stream_metadata, config, transformer)
+            LOGGER.info(state)
             singer.write_state(state)
 
     state = singer.set_currently_syncing(state, None)
